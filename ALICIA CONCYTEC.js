e4562502ec691abb6fa035417478637ebdfeb9bf
Eduardo Oros <eduardoom_@outlook.com>
2024-04-18 08:58:20 -0500

    Add ALICIA

---
+++ ALICIA CONCYTEC.js
@@ -0,0 +1,42 @@
+{
+	"translatorID": "bf1def78-e3c6-4df5-9710-e9677f9a6d52",
+	"label": "ALICIA CONCYTEC",
+	"creator": "Eduardo Oros",
+	"target": "^https?://(www\\\\.)?alicia.concytec.gob.pe/",
+	"minVersion": "5.0",
+	"maxVersion": "",
+	"priority": 100,
+	"inRepository": true,
+	"translatorType": 4,
+	"browserSupport": "gcsibv",
+	"lastUpdated": "2024-04-17 14:14:31"
+}
+
+function detectWeb(doc, url) {
+	const docTable = doc.querySelector("table.main-detail");
+	const itemType = text(docTable, "span", 1);
+	if (itemType === "tesis de grado" || itemType === "tesis de maestría" || itemType === "tesis doctoral") {
+		return "thesis";
+	} else if (itemType === "artículo") {
+		return "journalArticle";
+	} else if (itemType === "libro") {
+		return "book";
+	}
+
+	return false;
+}
+
+function doWeb(doc, url) {
+	const itemType = detectWeb(doc, url);
+	if (itemType === "thesis") {
+		const item = new Zotero.Item(itemType);
+		item.title = attr(doc, "meta[name='DC.title']", "content");
+		const creators = [];
+		attr(doc, "meta[name='DC.creator']", "content");
+		item.creators = []
+		item.complete();
+	}
+}/** BEGIN TEST CASES **/
+var testCases = [
+]
+/** END TEST CASES **/
---
+++ UCV.js
@@ -0,0 +1,37 @@
+{
+	"translatorID": "bf1def78-e3c6-4df5-9710-e9677f9a6d52",
+	"label": "UCV",
+	"creator": "Eduardo Oros",
+	"target": "^https?://(www\\\\.)?repositorio.ucv.edu.pe/",
+	"minVersion": "5.0",
+	"maxVersion": "",
+	"priority": 100,
+	"inRepository": true,
+	"translatorType": 4,
+	"browserSupport": "gcsibv",
+	"lastUpdated": "2024-04-17 13:07:55"
+}
+
+function detectWeb(doc, url) {
+	return "thesis";
+}
+
+function doWeb(doc, url) {
+	let itemType = detectWeb(doc, url);
+	if (itemType === "thesis") {
+		let translator = Zotero.loadTranslator("web");
+		translator.setTranslator("951c027d-74ac-47d4-a107-9c3069ab7b48")
+		translator.setDocument(doc);
+		translator.setHandler("itemDone", function (obj, item) {
+			item.itemType = "thesis";
+			item.complete();
+		})
+
+		translator.getTranslatorObject(function (trans) {
+			trans.doWeb(doc, url);
+		});
+	}
+}/** BEGIN TEST CASES **/
+var testCases = [
+]
+/** END TEST CASES **/
